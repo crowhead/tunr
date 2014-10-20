4179b5e70da149283ddc5f30be25e1348a879a49
crowhead <maxlewis0@gmail.com>
2014-10-20 16:29:30 -0400

    Tunr
    added new tests for update and the code to make it work, did the same for a show function

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,5 +1,43 @@
 class AlbumsController <ApplicationController
+  before_filter :set_album, only: [:show, :edit, :update]
+
   def index
     @albums = Album.all
   end
-end
+
+  def new
+    @album = Album.new
+  end
+
+  def create
+    @album = Album.new(album_params)
+    if @album.save
+      redirect_to @album, notice: 'Album was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @album.update(album_params)
+      redirect_to @album, notice: 'Album was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  private
+    def set_album
+        @album = Album.find(params[:id])
+    end
+    def album_params
+        params.require(:album).permit(:title, :artist, :year)
+    end
+  end
+
--- spec/features/managing_albums_spec.rb
+++ spec/features/managing_albums_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
+ def create_astrolounge
+        Album.create!(
+     title: 'Astrolounge',
+     artist: 'Smash Mouth',
+     year: '1999'
+    )
+    end
+
 feature "Manage Albums" do
   scenario "List no Albums" do
     visit albums_path
@@ -17,7 +25,30 @@ feature "Manage Albums" do
       expect(page.find('.album')).to have_content(/Astrolounge/)
       expect(page.find('.artist')).to have_content(/Smash Mouth/)
       expect(page.find('.year')).to have_content(/1999/)
+      expect(page).not_to have_content(/No albums available/i)
   end
 
-  scenario "Add new Album"
+  scenario "Add new Album" do
+      visit new_album_path
+      fill_in 'Title', with: 'Astrolounge'
+      fill_in 'Artist', with: 'Smash Mouth'
+      fill_in 'Year', with: '1999'
+      click_on 'Create Album'
+      expect(current_path).to eq(album_path(Album.last.id))
+      expect(find('.notice')).to have_content(/success/i)
+  end
+
+  scenario "Update an Album" do
+
+    album = create_astrolounge
+    visit edit_album_path(album)
+      fill_in 'Title', with: 'Smash Mouth'
+      fill_in 'Year', with: '2001'
+      click_on 'Update Album'
+
+      expect(current_path).to eq(album_path(album))
+      expect(find('.notice')).to have_content(/updated/i)
+  end
 end
+
+
