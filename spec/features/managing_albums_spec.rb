beed8a3493d6fe23e2c79894eff16ae7daf24a6a
stacybanks514 <stacybanks514@gmail.com>
2014-10-21 16:00:40 -0400

    Adding users

--- spec/features/managing_albums_spec.rb
+++ spec/features/managing_albums_spec.rb
@@ -11,17 +11,16 @@ end
 feature "Manage Albums" do
   scenario "List no Albums" do
     visit albums_path
-    expect(page.find('h1')).to have_content /Albums/
+
+    expect(page.find('h1')).to have_content(/Albums/)
     expect(page).to have_content(/No albums available/i)
   end
 
   scenario "List all Albums" do
-    Album.create!(
-      title: 'Astrolounge',
-      artist: 'Smash Mouth',
-      year: '1999'
-    )
+    create_astrolounge
+
     visit albums_path
+
     expect(page.find('.album')).to have_content(/Astrolounge/)
     expect(page.find('.artist')).to have_content(/Smash Mouth/)
     expect(page.find('.year')).to have_content(/1999/)
@@ -34,13 +33,14 @@ feature "Manage Albums" do
     fill_in 'Artist', with: 'Smash Mouth'
     fill_in 'Year', with: '1999'
     click_on 'Create Album'
+
     expect(current_path).to eq(album_path(Album.last.id))
     expect(find('.notice')).to have_content(/success/i)
   end
 
   scenario "Update an Album" do
-
     album = create_astrolounge
+
     visit edit_album_path(album)
     fill_in 'Title', with: 'Smash Mouth'
     fill_in 'Year', with: '2001'
@@ -52,15 +52,19 @@ feature "Manage Albums" do
 
   scenario "Show an Album" do
     album = create_astrolounge
+
     visit album_path(album)
+
     expect(current_path).to eq(album_path(album))
   end
 
   scenario "Remove an Album" do
-    album = create_astrolounge
+    create_astrolounge
+    
     visit albums_path
-    # click_on 'Destroy'
-    expect { click_link('Destroy') }.to change(Albums, :count).by(-1)
-  end
+    click_on 'Remove'
 
+    expect(current_path).to eq(albums_path)
+    expect(find('.notice')).to have_content(/deleted/i)
+  end
 end
